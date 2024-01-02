3c473244e857063cb5bf964481f6fb834846dc0b
Oleh Demchenko <blacklife1992@gmail.com>
2024-01-02 02:54:57 +0200

    78/96 Iterators

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -15,7 +13,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) =>  first.to_uppercase().chain(c).collect(),
     }
 }
 
@@ -24,7 +22,15 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    // Variant #1
+    // let mut v = Vec::new();
+    // for i in words.iter() {
+    //     v.push(capitalize_first(i));
+    // }
+    // v
+
+    // Variant #2
+    words.iter().map(|word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -32,7 +38,15 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    // Variant #1
+    // let mut s = String::new();
+    // for w in words.iter() {
+    //     s = s + capitalize_first(w).as_str();
+    // }
+    // s
+
+    // Variant #2
+    words.iter().map(|word| capitalize_first(word)).collect()
 }
 
 #[cfg(test)]
--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -11,11 +11,9 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Progress {
     None,
     Some,
@@ -35,7 +33,10 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    // todo!();
+    map.into_iter()
+        .filter(|(_, progress)| progress == &&value)
+        .count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +55,9 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    // todo!();
+    dbg!(collection);
+    collection.iter().fold(0, |acc, x| acc + count_iterator(x, value))
 }
 
 #[cfg(test)]
